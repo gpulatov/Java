d53b94ea9b77398d84cb7d59d3a96149ae8bab96
gpulatov <gpulatov@gmail.com>
2018-12-15 12:20:20 -0600

    Assignment use Scanner

--- Assignments/src/Assign22Printer.java
+++ Assignments/src/Assign22Printer.java
@@ -1,6 +1,32 @@
 
 public class Assign22Printer {
+	
 	private int tonerLevel;
 	private int numPagesPrinted;
-	private int num
+	
+	public Assign22Printer(int tonerLevel, int numPagesPrinted) {
+		
+		this.tonerLevel = tonerLevel;
+		this.numPagesPrinted = numPagesPrinted;
+		
+	}
+
+	public int getTonerLevel() {
+		return tonerLevel;
+	}
+
+	public int getNumPagesPrinted() {
+		return numPagesPrinted;
+	}
+	
+	public void fillUpToner () {
+		if(getTonerLevel()==0) {
+			tonerLevel++;
+			tonerLevel*=100;
+		}
+	}
+	public void pagesPrinted () {
+		this.numPagesPrinted++;
+	}
+	
 }
