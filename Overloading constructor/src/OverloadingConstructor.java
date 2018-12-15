455a61e7fdb8c55c5b426ff84ccadc3f1af37350
gpulatov <gpulatov@gmail.com>
2018-12-15 14:46:34 -0600

    Overloading Constructor

---
+++ Overloading constructor/src/OverloadingConstructor.java
@@ -0,0 +1,19 @@
+
+public class OverloadingConstructor {
+
+	public static void main(String[] args) {
+		
+		Car c1= new Car("Toyota","Corolla",2017);
+//		c1.make="Honda";
+//		c1.model="Civic";
+		
+//		Car c2=new Car();
+//		c2.make="Toyota";
+//		c2.model="Corolla";
+		
+//		c1.start();
+//		c2.start();
+
+	}
+
+}
