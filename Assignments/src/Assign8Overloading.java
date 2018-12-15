e9d7baabaab0e5a03b4d3d549105af891321955b
gpulatov <gpulatov@gmail.com>
2018-12-15 13:37:12 -0600

    Change on file name

---
+++ Assignments/src/Assign8Overloading.java
@@ -0,0 +1,28 @@
+
+public class Assign8Overloading {
+
+	public static void main(String[] args) {
+		System.out.println(getDurationString(6850));
+		System.out.println(getDurationString(135, 57));
+	}
+
+	public static String getDurationString(int minutes, int seconds) {
+		if (minutes < 0 || (seconds < 0 && seconds > 59)) {
+			return "Invalid Value";
+		}
+		int hour = minutes / 60;
+		int remain = minutes % 60;
+		return hour + "h " + remain + "m " + seconds + "s";
+	}
+
+	public static String getDurationString(int seconds) {
+		if (seconds < 0) {
+			return "Invalid Value";
+		}
+		
+		int minutes = seconds/60;
+		int remainSec = seconds%60;
+		System.out.println(minutes+"m "+remainSec+"s");
+		return (getDurationString(minutes, remainSec));
+	}
+}
